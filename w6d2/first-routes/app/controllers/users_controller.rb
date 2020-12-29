7102071b96401fc28bbb93a465b1fbb28342ec86
Qi Zhu <rubybfm1017@gmail.com>
2020-12-29 15:20:33 -0800

    post

--- w6d2/first-routes/app/controllers/users_controller.rb
+++ w6d2/first-routes/app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
     def index
-        # render json: User.all
-        render plain: "I'm in the index action!"
+         render json: User.all
+        # render plain: "I'm in the index action!"
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:email,:name))
+        user.save!
+        render json: user
+    end
+
+    def show
+        render json:params
     end
 end
