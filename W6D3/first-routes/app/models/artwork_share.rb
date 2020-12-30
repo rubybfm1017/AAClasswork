351a4f49768fe6b7176c17d141d466f773b9eb84
IsoVoyd <dubnersamuel@gmail.com>
2020-12-30 15:04:02 -0800

    create seeds.rb

--- W6D3/first-routes/app/models/artwork_share.rb
+++ W6D3/first-routes/app/models/artwork_share.rb
@@ -1,4 +1,22 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :bigint           not null, primary key
+#  artwork_id :integer          not null
+#  viewer_id  :integer          not null
+#
 class ArtworkShare < ApplicationRecord
     validates :viewer_id, presence: true
-    validates :artwork_id, presence: true, uniqueness: {scope: :viewer_id}
+    validates :artwork_id, presence: true, uniqueness: { scope: :viewer_id }
+
+    belongs_to :artwork,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :Artwork
+
+    belongs_to :viewer,
+        primary_key: :id,
+        foregin_key: :viewer_id,
+        class_name: User
 end
--- W6D3/first-routes/db/seeds.rb
+++ W6D3/first-routes/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cat = User.create(username: "cat")
+dog = User.create(username: "dog")
+# User.create(username: "cat")
+
+starry_night = Artwork.create(title: "Starry Night", image_url: "cat.com", artist_id: cat.id)
+coffee_shop = Artwork.create(title: "Coffee Shop", image_url: "dog.com", artist_id: dog.id)
+
+ArtworkShare.create(artwork_id: starry_night.id, viewer_id: dog.id)
+ArtworkShare.create(artwork_id: coffee_shop.id, viewer_id: cat.id)
